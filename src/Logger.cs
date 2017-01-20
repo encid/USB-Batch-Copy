de32fcd1d08a9fba10696b62f531248d87e5d371
encid <ryancav@gmail.com>
2017-01-20 12:57:24 -0500

    fixed logger color bleed

--- src/Logger.cs
+++ src/Logger.cs
@@ -7,19 +7,25 @@ namespace USBBatchCopy
     public static class Logger {
 
         /// <summary>
-        /// Write a message to the status log textbox.
+        /// Write a message to the status log textbox, with default black foreground text color.
         /// </summary>
         /// <param name="message">Message to write to the status log.</param>
-        /// <param name="box">Object to target.</param>
+        /// <param name="box">RichTextBox control to use for writing</param>
         public static void Log(string message, RichTextBox box)
         {
             if (string.IsNullOrEmpty(box.Text)) {
-                box.AppendText(message);
+                box.AppendText(message, Color.Black);
             }
             else
-                box.AppendText(Environment.NewLine + message);
+                box.AppendText(Environment.NewLine + message, Color.Black);
         }
 
+        /// <summary>
+        /// Write a message to the status log textbox, a specified foreground text color.
+        /// </summary>
+        /// <param name="message">Message to write to the status log.</param>
+        /// <param name="box">RichTextBox control to use for writing.</param>
+        /// <param name="color">Foreground color of text.</param>
         public static void Log(string message, RichTextBox box, Color color)
         {
             if (string.IsNullOrEmpty(box.Text)) {
